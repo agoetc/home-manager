16351c49518dc6ff73c75408d348c5ab32675cd9
agoetc <tkgw666private@gmail.com>
2024-07-23 20:07:33 +0900

    change state version to 24.05

--- home.nix
+++ home.nix
@@ -8,7 +8,7 @@
   home = {
     username = "takegawa";
     homeDirectory = "/Users/takegawa";
-    stateVersion = "23.11"; # Please read the comment before changing.
+    stateVersion = "24.05";
   };
 
 
