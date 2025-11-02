c7ac22e5472ec405d0cb73dcf7b818eb17cbeb8e
agoetc <tkgw666private@gmail.com>
2025-11-02 11:24:28 +0900

    update

--- config/git.nix
+++ config/git.nix
@@ -11,13 +11,15 @@
       "tsconfig.tsbuildinfo"
       ".DS_Store"
     ];
-    aliases = {
-      # merge済みブランチを削除
-      cleanup = "!git branch --merged | grep -v '\\*\\|main\\|master\\|develop' | xargs -n 1 git branch -d";
-      # リモートで削除されたブランチのローカル追跡ブランチを削除
-      prune-local = "!git remote prune origin && git branch -vv | grep ': gone]' | awk '{print $1}' | xargs -n 1 git branch -D";
-      # 両方を実行
-      cleanup-all = "!git cleanup && git prune-local";
+    settings = {
+      alias = {
+        # merge済みブランチを削除
+        cleanup = "!git branch --merged | grep -v '\\*\\|main\\|master\\|develop' | xargs -n 1 git branch -d";
+        # リモートで削除されたブランチのローカル追跡ブランチを削除
+        prune-local = "!git remote prune origin && git branch -vv | grep ': gone]' | awk '{print $1}' | xargs -n 1 git branch -D";
+        # 両方を実行
+        cleanup-all = "!git cleanup && git prune-local";
+      };
     };
   };
 }
