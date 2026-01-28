38f17d4d0acf36c9b8ce460648b31c2de38b5dfa
agoetc <tkgw666private@gmail.com>
2026-01-28 22:51:30 +0900

    update

--- config/zsh.nix
+++ config/zsh.nix
@@ -130,6 +130,7 @@
       # iTerm2 Shell Integration
       # ============================================
       test -e "$HOME/.iterm2_shell_integration.zsh" && source "$HOME/.iterm2_shell_integration.zsh"
+
     '';
   };
 
